8ca50a15affcc3f254134890708495ed3fe4f6d6
Digv06 <86612866+Digv06@users.noreply.github.com>
2024-01-23 20:14:15 +0530

    added refreshing refresh token route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -28,5 +28,6 @@ router.route("/login").post(userLogin);
 
 // secured routes we are using the verify middleware to check the existance of user and then next to userLogout
 router.route("/logout").post(verifyJWT, userLogout);
+router.route("/refresh-token").post(refreshingRefreshToken);
 
 export default router;
